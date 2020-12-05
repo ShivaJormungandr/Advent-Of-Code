667058af6a82c6c7647ec5c25e7d0021d9d47257
ShivaJormungandr <56564606+ShivaJormungandr@users.noreply.github.com>
2020-12-05 12:06:21 +0200

    Day05

---
+++ 2020/Day05/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.IO;
+
+namespace Day05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxRows = 127, maxColums = 7;
+            int maxSeat = 0, minSeat = (maxRows * 8 + maxColums);
+            bool[,] allSeats = new bool[maxRows + 1, maxColums + 1];
+            for (int i = 0; i <= maxRows; i++)
+            {
+                for (int j = 0; j <= maxColums; j++)
+                {
+                    allSeats[i, j] = false;
+                }
+            }
+
+            StreamReader sr = new StreamReader(@"..\..\..\in.txt");
+            string line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                int minCurrentRow = 0, maxCurrentRow = maxRows;
+                int minCurrentColumn = 0, maxCurretColumn = maxColums;
+                foreach (char c in line)
+                {
+                    if (c.Equals('F'))
+                    {
+                        maxCurrentRow = maxCurrentRow / 2 + minCurrentRow / 2;
+                    }
+                    else if (c.Equals('B'))
+                    {
+                        minCurrentRow = maxCurrentRow / 2 + minCurrentRow / 2 + 1;
+                    }
+                    else if (c.Equals('L'))
+                    {
+                        maxCurretColumn = maxCurretColumn / 2 + minCurrentColumn / 2;
+                    }
+                    else if (c.Equals('R'))
+                    {
+                        minCurrentColumn = maxCurretColumn / 2 + minCurrentColumn / 2 + 1;
+                    }
+                }
+                int curentSeat = maxCurrentRow * 8 + maxCurretColumn;
+                allSeats[maxCurrentRow, maxCurretColumn] = true;
+                if (curentSeat > maxSeat)
+                {
+                    maxSeat = curentSeat;
+                }
+                if (curentSeat < minSeat)
+                {
+                    minSeat = curentSeat;
+                }
+            }
+
+            Console.WriteLine(maxSeat);
+
+            bool ok = false;
+            for (int i = 0; i <= maxRows; i++)
+            {
+                for (int j = 0; j <= maxColums; j++)
+                {
+                    if ((i * 8 + j) > minSeat && (i * 8 + j) < maxSeat && allSeats[i, j] == false)
+                    {
+                        Console.WriteLine(i * 8 + j);
+                        ok = true;
+                        break;
+                    }
+                }
+                if (ok)
+                {
+                    break;
+                }
+            }
+
+        }
+
+    }
+}
